e1b303eae277cd3dbe29295e3e2496b8d029d2c1
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2025-05-28 19:00:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ToDoList from "./components/ToDoList/ListaTarefas"
+import './main.css'
+export default function App(){
+  return(
+      <div className="body">
+        <ToDoList/>
+      </div>
+    )
+}
